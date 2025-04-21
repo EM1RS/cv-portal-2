4fc3900463f00cd8c1643c1555cbcd70a9cf0445
EM1RS <emirsokoli@gmail.com>
2025-04-21 20:53:13 +0200

    Mid commit før rebase

---
+++ bin/Release/net8.0/Migrations/20250413101600_UpdatedSomClasses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CvApi2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedSomClasses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Skill_name",
+                table: "CompetenceOverview",
+                newName: "skill_name");
+
+            migrationBuilder.RenameColumn(
+                name: "Skill_level",
+                table: "CompetenceOverview",
+                newName: "skill_level");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "ProjectExperience",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "ProjectExperience",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "ProjectExperience");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "ProjectExperience");
+
+            migrationBuilder.RenameColumn(
+                name: "skill_name",
+                table: "CompetenceOverview",
+                newName: "Skill_name");
+
+            migrationBuilder.RenameColumn(
+                name: "skill_level",
+                table: "CompetenceOverview",
+                newName: "Skill_level");
+        }
+    }
+}
